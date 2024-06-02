eb891bf42ccdcf02fc4a3ad929a9580853878918
AritriPodde2210 <123970201+AritriPodde2210@users.noreply.github.com>
2024-06-02 15:36:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,101 @@
+"use strict";
+
+const start = async () => {
+  let algoValue = Number(document.querySelector(".algo-menu").value);
+  let speedValue = Number(document.querySelector(".speed-menu").value);
+
+  if (speedValue === 0) {
+    speedValue = 1;
+  }
+  if (algoValue === 0) {
+    alert("No Algorithm Selected");
+    return;
+  }
+
+  let algorithm = new sortAlgorithms(speedValue);
+  switch (algoValue) {
+    case 1:
+      await algorithm.BubbleSort();
+      break;
+    case 2:
+      await algorithm.SelectionSort();
+      break;
+    case 3:
+      await algorithm.InsertionSort();
+      break;
+    case 4:
+      await algorithm.MergeSort();
+      break;
+    case 5:
+      await algorithm.QuickSort();
+      break;
+  }
+};
+
+const RenderScreen = async () => {
+  await RenderList();
+};
+
+const RenderList = async () => {
+  let sizeValue = Number(document.querySelector(".size-menu").value);
+  await clearScreen();
+
+  let list = await randomList(sizeValue);
+  const arrayNode = document.querySelector(".array");
+
+  for (const element of list) {
+    const node = document.createElement("div");
+    node.className = "cell";
+    node.setAttribute("value", String(element));
+    node.style.height = `${3.8 * element}px`;
+    arrayNode.appendChild(node);
+  }
+};
+
+const RenderArray = async (sorted) => {
+  let sizeValue = Number(document.querySelector(".size-menu").value);
+  await clearScreen();
+
+  let list = await randomList(sizeValue);
+  if (sorted) list.sort((a, b) => a - b);
+
+  const arrayNode = document.querySelector(".array");
+  const divnode = document.createElement("div");
+  divnode.className = "s-array";
+
+  for (const element of list) {
+    const dnode = document.createElement("div");
+    dnode.className = "s-cell";
+    dnode.innerText = element;
+    divnode.appendChild(dnode);
+  }
+  arrayNode.appendChild(divnode);
+};
+
+const randomList = async (Length) => {
+  let list = [];
+  let lowerBound = 1;
+  let upperBound = 100;
+
+  for (let counter = 0; counter < Length; ++counter) {
+    let randomNumber = Math.floor(Math.random() * (upperBound - lowerBound + 1) + lowerBound);
+    list.push(randomNumber);
+  }
+  return list;
+};
+
+const clearScreen = async () => {
+  document.querySelector(".array").innerHTML = "";
+};
+
+const response = () => {
+  let Navbar = document.querySelector(".navbar");
+  Navbar.className = (Navbar.className === "navbar") ? "navbar responsive" : "navbar";
+};
+
+// Event Listeners
+document.querySelector(".icon").addEventListener("click", response);
+document.querySelector(".start").addEventListener("click", start);
+document.querySelector(".size-menu").addEventListener("change", RenderScreen);
+document.querySelector(".algo-menu").addEventListener("change", RenderScreen);
+window.onload = RenderScreen;
